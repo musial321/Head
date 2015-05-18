bbdb7998c72a4656f0264f048a08a86c6477267a
Christian Musial <musial321@gmail.com>
2015-05-18 12:46:52 -0400

    Revised head project

--- Head/src/Head.java
+++ Head/src/Head.java
@@ -1,43 +1,38 @@
 import java.io.*;
-import java.util.Scanner;
 
-public class Head {
-	
+public class Head 
+{
+	static File input;
+	static int n=10;
 	
 	public static boolean parseArgs(String [] args)
 	{
 		boolean bool = true;
-		
 		char arg1_0=' ';
 		char arg1_1=' ';
 		String [] finalArgs = new String[3];
 		
-		
-		
 		if(args.length==3)
 		{
 			finalArgs = args;
+			
 		}
 		if(args.length==1)
 		{
 			finalArgs[0]="-n";
 			finalArgs[1]=Integer.toString(10);
 			finalArgs[2]=args[0];
-			
 		}
-		
 		
 		try
 		{
-		arg1_0 = finalArgs[0].charAt(0);
-		arg1_1 = finalArgs[0].charAt(1);
-		
+			arg1_0 = finalArgs[0].charAt(0);
+			arg1_1 = finalArgs[0].charAt(1);
 		}
 		catch(Exception e)
 		{
 			bool=false;
 		}
-		
 		
 		if(arg1_0 != '-' || arg1_1 != 'n')		
 		{	
@@ -48,28 +43,26 @@ public class Head {
 		try
 		{
 			finalArgs[1] = Integer.toString((Integer.parseInt(finalArgs[1])));
+			n = Integer.parseInt(finalArgs[1]);
 			
-			if(Integer.parseInt(finalArgs[1])<1)
+			if(n<1)
 			{
 				bool = false;
-				
 			}
 		}
 		catch(Exception e)
 		{
-			
 			bool = false;
-		
 		}
 		
 		try
 		{
-		File input = new File(finalArgs[2]);
-		if(!input.exists())
+			input = new File(finalArgs[2]);
+		
+			if(!input.exists())
 			{
 				bool = false;
 			}
-			
 		}
 		catch(Exception e)
 		{
@@ -79,104 +72,44 @@ public class Head {
 		return bool;
 		}
 	
-	
 	public static void main(String [] args) throws IOException
 	{
-		
-		
+		boolean correctArgs;
+		correctArgs = parseArgs(args);
 	
-	boolean correctArgs;
-	correctArgs = parseArgs(args);
-	
-	
-	if(!correctArgs)
-	{
-		System.out.println("Argument error.");
-	}
-		
-	if(correctArgs)
-	{
-	
-	BufferedReader in = null;
-	BufferedWriter out = null;
-	
-	
-	String str = "";
-	
-	Scanner s = new Scanner(System.in);
-	
-	File input;
-	
-	if(args.length==3)
-	{
-	input = new File(args[2]);
-	}
-	else
-	{
-	input = new File(args[0]);
-	}	
-	
-	//File output = new File("output.txt");
-	
-	in = new BufferedReader(new FileReader(input));
-	//out = new BufferedWriter(new FileWriter(output));
-	
-	
-	try
-	{
-		int i = 0;
-		
-		if(args.length==3)
+		if(!correctArgs)
 		{
-		while(i<Integer.parseInt(args[1]))
+			System.out.println("Argument error.");
+		}
+		if(correctArgs)
 		{
-			str = in.readLine();
-			
-			if(str!=null)
+			BufferedReader in = null;
+			in = new BufferedReader(new FileReader(input));
+			String str = "";
+	
+			try
 			{
-				System.out.println(str);
+					int i = 0;
+		
+					while(i<n)
+					{
+						str = in.readLine();
+			
+						if(str!=null)
+						{
+							System.out.println(str);
+						}
+						
+						i++;
+					}		
 			}
-			
-			i++;
-		}
-		}
-		else
-		{
-			while(i<10)
+			finally
 			{
-				str = in.readLine();
-				
-				if(str!=null)
+				if (in!=null)
 				{
-					System.out.println(str);
+					in.close();
 				}
-				
-				i++;
 			}
 		}
 	}
-	finally
-	{
-		if (in!=null)
-		{
-			in.close();
-		}
-		if(out!=null)
-		{
-			out.close();
-		}
-	}
-	
-	
-	
-
-
-	}
-
-
 }
-	
-}
-
-
-
